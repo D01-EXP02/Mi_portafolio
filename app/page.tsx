14509bee0617c15791a6ec58878617652c861006
D01-EXP02 <espitiaesteban.es@hotmail.com>
2026-06-16 17:33:44 -0500

    "Actualizacion de Navbar, cambios en la visualizacion de la pagina, se agregan nuevas imagenes y se actualizan los datos del portafolio"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import CoverParticle from "@/component/coverParticle";
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-gradient-cover">
+    <main className="relative min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
       <CoverParticle />
-      <div className="relative z-10 flex flex-col justify-center min-h-screen px-10">
+      <div className="relative flex flex-col justify-center min-h-screen px-10">
         <h1 className="text-white text-5xl font-bold">Hola, soy David</h1>
         <p className="text-yellow-400 text-xl mt-4">Mi intro</p>
       </div>
